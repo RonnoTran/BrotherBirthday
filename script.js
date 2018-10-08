67c9c91a96c852473e63a47714b724d53875ee8d
RonnoTran <cbod.devil@gmail.com>
2018-10-08 16:14:41 -0700

    added Brother's birthday project

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPresent() {
+	let h1 = document.getElementsByClassName('rainbow')[0];
+	h1.innerHTML = "It is coming in 2 weeks or so";
+	let img = document.getElementById('gif');
+	img.src = "haha.gif";
+	img.height = "200";
+}
